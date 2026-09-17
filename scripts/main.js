ffca77f0947868f6dcf5ae68f45926539afa53ac
KirbVEVO <morganl3469@gmail.com>
2026-09-18 00:27:51 +0100

    main js  changes

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ class AnnwnCalendarApp extends HandlebarsApplicationMixin(ApplicationV2) {
   };
 
   static PARTS = {
-    calendar: { template: "modules/Annwn-alendar/templates/calendar.html" }
+    calendar: { template: "modules/Annwn-Calendar/templates/calendar.html" }
   };
 }
 
