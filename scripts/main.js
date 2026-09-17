a53a90c4554efc6b21c5f28df0cd5d0d953c7c1f
KirbVEVO <morganl3469@gmail.com>
2026-09-17 23:41:32 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// scripts/main.js
+const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api;
+
+class AnnwnCalendarApp extends HandlebarsApplicationMixin(ApplicationV2) {
+  static DEFAULT_OPTIONS = {
+    id: "annwn-calendar-app",
+    window: { title: "Annwn Calendar" },
+    position: { width: 900, height: 700 }
+  };
+
+  static PARTS = {
+    calendar: { template: "modules/annwn-calendar/templates/calendar.html" }
+  };
+}
+
+Hooks.once("ready", () => {
+  game.annwnCalendar = new AnnwnCalendarApp();
+});
